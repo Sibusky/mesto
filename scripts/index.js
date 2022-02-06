cc2eda9496652b371fa005f627d27c0b69c040e7
Alexey Smirnov <Sibusky@yandex.ru>
2022-02-06 15:37:34 +0300

    fix JS code

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,39 @@
-const profileEditButton = document.querySelector('.profile__edit-button')
-const profilePopupEdit = document.querySelector('.popup')
-const profilePopupEditClose = document.querySelector('.popup__close-button')
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profilePopupEdit = document.querySelector('.popup');
+const profilePopupEditClose = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__name');
+const bioInput = document.querySelector('.popup__bio');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
 
-
+// Функция открытия popup. Присваивает класс 'popup_opened' со свойством display: flex,
+// и вставляет данные из профиля в popup.
 function openProfilePopupEdit() {
-    profilePopupEdit.classList.add('popup_opened')
-}
+    profilePopupEdit.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    bioInput.value = profileBio.textContent;
+};
 
+// Функция закрытия popup; удаляет класс 'popup_opened'.
 function closeProfilePopupEdit() {
-    profilePopupEdit.classList.remove('popup_opened')
-}
+    profilePopupEdit.classList.remove('popup_opened');
+};
 
-
-profileEditButton.addEventListener('click', openProfilePopupEdit)
-profilePopupEditClose.addEventListener('click', closeProfilePopupEdit)
-
-profilePopupEdit.addEventListener('click', function(event) {
+// Функция закрытия popup при клике вне окна. Работает не вовсем корректно:
+// при нажатии в области окна, а отпускании вне, окно закроется,
+// что может привести к ложным закрытиям окна при выделении текста в input
+/* profilePopupEdit.addEventListener('click', function(event) {
     if(event.target === event.currentTarget) {
         closeProfilePopupEdit()
     }
-})
+}); */
 
 
-let formElement = document.querySelector('.popup__container')  
-let nameInput = document.querySelector('.popup__name')   
-let bioInput = document.querySelector('.popup__bio')
-let profileName = document.querySelector('.profile__name')
-let profileBio = document.querySelector('.profile__bio')
-
+// Функция отправки формы:
+// preventDefault() - убирает дефолтные действия движка (в данном случае - обновление страницы),
+// дальше - вставляем данные формы в профиль,
+// в конце - закрываем окно редактирования (popup).
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value;
@@ -35,4 +41,7 @@ function formSubmitHandler (evt) {
     closeProfilePopupEdit();
 }
 
+// Добавляю слушателей событий
+profileEditButton.addEventListener('click', openProfilePopupEdit);
+profilePopupEditClose.addEventListener('click', closeProfilePopupEdit);
 formElement.addEventListener('submit', formSubmitHandler);
