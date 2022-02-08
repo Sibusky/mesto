c75b8f2ff780d9ff3b7df323fd22bea4f00105ef
Alexey Smirnov <Sibusky@yandex.ru>
2022-02-08 17:42:58 +0300

    at the beginning of next sprint

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,32 @@ const nameInput = document.querySelector('.popup__input_type_name');
 const bioInput = document.querySelector('.popup__input_type_bio');
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
+const initialCards = [
+    {
+      name: 'Северная Осетия',
+      link: 'https://images.unsplash.com/photo-1612719734820-81784b7e6573?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
 
 // Функция открытия popup. Присваивает класс 'popup_opened' со свойством display: flex,
 // и вставляет данные из профиля в popup.
